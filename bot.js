df3ebfb2dcbff3f21fb825dd637c241907b0afc7
MOHAMEDJJJJAK <52308633+MOHAMEDJJJJAK@users.noreply.github.com>
2019-07-04 19:18:19 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`فارس كتير كيوت`,"http://twitch.tv/فارس كتير كيوت")
+client.user.setStatus("dnd")
+});
+
+client.login('NTg1NTE1OTkxMzkyNTgzNjgy.XR4vdw.tzpj7M1gUn0aLGNPhUQA8rFAIAs');// لا تغير فيها شيء
